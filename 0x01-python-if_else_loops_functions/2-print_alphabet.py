3fe1a21904e3a06df2be2baaa2b086ede6bcd3b4
Nj3r1 <susannganga884@gmail.com>
2024-01-04 12:10:19 +0300

    alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python4
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print("{:c}".format(alpha_letters), end="")
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i), end='')
+
