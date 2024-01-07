11b869d0137eab8004faeaa5c41695a4f5db2d74
Nj3r1 <susannganga884@gmail.com>
2024-01-07 16:16:25 +0300

    a list of intergers

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python4
 for letter in range(97, 122):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
         print("{}".format(chr(letter)), end="")
---
+++ 0x03-python-data_structures/0-print_list_interger.py
@@ -0,0 +1,5 @@
+ #!/usr/bin/python3
+  2
+  3 def print_list_interger(my_list=[]):
+  4         for num in my_list:
+  5         print("{}".format(num))
