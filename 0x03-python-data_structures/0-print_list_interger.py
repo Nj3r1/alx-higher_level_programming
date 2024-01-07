0e999fd96e3f4fcc1eda61523c2b2813f8435f1d
Nj3r1 <susannganga884@gmail.com>
2024-01-07 16:19:57 +0300

    a list of intergers

--- 0x03-python-data_structures/0-print_list_interger.py
+++ 0x03-python-data_structures/0-print_list_interger.py
@@ -1,5 +1,5 @@
  #!/usr/bin/python3
-  2
-  3 def print_list_interger(my_list=[]):
-  4         for num in my_list:
-  5         print("{}".format(num))
+
+def print_list_interger(my_list=[]):
+	for num in my_list:
+	print("{}".format(num))
